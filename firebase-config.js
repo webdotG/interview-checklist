0bf30068d0b7a24d31e436a3188c16cd5fc45adf
webdotG <webdotg@gmail.com>
2025-07-31 19:58:10 +0300

    Add firebase config for GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAQCgDpHF9u2i6swE0j0lNxiZmRp9j42oE',
+  authDomain: 'interview-checklist.firebaseapp.com',
+  projectId: 'interview-checklist',
+  storageBucket: 'interview-checklist.appspot.com',
+  messagingSenderId: '1038665174709',
+  appId: '1:1038665174709:web:4920e8a7edd6a9c2c938fc',
+}
+
+export default firebaseConfig
