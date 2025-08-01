b8965b4610d78f64c1eacf9436f1448dc21089ff
webdotG <webdotg@gmail.com>
2025-08-01 23:05:16 +0300

    conf fb fix

--- FirebaseService.js
+++ FirebaseService.js
@@ -2,6 +2,15 @@ export class FirebaseService {
   constructor() {
     this.firestore = null
     this.initialized = false
+
+    this.firebaseConfig = {
+      apiKey: 'AIzaSyAQCgDpHF9u2i6swE0j0lNxiZmRp9j42oE',
+      authDomain: 'interview-checklist.firebaseapp.com',
+      projectId: 'interview-checklist',
+      storageBucket: 'interview-checklist.appspot.com',
+      messagingSenderId: '1038665174709',
+      appId: '1:1038665174709:web:4920e8a7edd6a9c2c938fc',
+    }
   }
 
   async initialize() {
@@ -19,8 +28,7 @@ export class FirebaseService {
         'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js'
       )
 
-      const firebaseConfig = await import('./firebase-config.js')
-      const firebaseApp = initializeApp(firebaseConfig.default)
+      const firebaseApp = initializeApp(this.firebaseConfig)
       this.firestore = getFirestore(firebaseApp)
       this.initialized = true
 
